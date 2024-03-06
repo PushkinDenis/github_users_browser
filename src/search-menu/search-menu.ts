077f4bceb24a91dc25f90eee501fa40bb3c2d8f0
Denis <denisp9831@gmail.com>
2024-03-06 14:12:33 +1000

    header landing finished

--- src/search-menu/search-menu.ts
+++ src/search-menu/search-menu.ts
@@ -2,7 +2,7 @@ const searchMenu = (): string => {
   return `<div class="search">
     <div class="search__text">Search: </div>
     <div class="search__input-wrapper">
-      <input type="text" class="search__input">
+      <input type="text" class="search__input" placeholder = "Find user by name">
     </div>
     <button class="search__button">Find user</button>
   </div>`;

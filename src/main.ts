d7825f79976b2db1ca0d6bcb2cc9cb2c05b35c61
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-03-08 00:05:37 +1000

    findName function created

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
 import { header } from "./header/header";
+import { findName } from "./search-menu/search-menu.ts";
+
 header();
+findName();
--- src/search-menu/search-menu.ts
+++ src/search-menu/search-menu.ts
@@ -9,3 +9,26 @@ const searchMenu = (): string => {
 };
 
 export { searchMenu };
+
+const findUser = async (userName: string): Promise<void> => {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+  const user: object = await response.json();
+  console.log(user.avatar_url);
+  document.body.insertAdjacentHTML("beforeend", `<img src="${user.avatar_url}" alt="avatar">`);
+};
+
+const findName = (): void => {
+  const searchButtons: NodeListOf<Element> = document.querySelectorAll(".search__button");
+  for (let searchButton of searchButtons) {
+    searchButton.addEventListener("click", (btn: Event): void => {
+      const searchBtnTarg = btn.target as HTMLElement;
+      const searchInp = searchBtnTarg.previousElementSibling.firstElementChild as HTMLInputElement;
+      const searchInpVal: string = searchInp.value;
+      if (searchInpVal !== "") {
+        findUser(searchInpVal);
+      }
+    });
+  }
+};
+
+export { findName };

26859e4a86fd82181e87dc27535bea0bb351f4ad
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-03-16 21:42:46 +1000

    serch-menu-helpers fixed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { header } from "./header/header";
 import { createUserCard } from "./search-menu/search-menu-helpers";
 import { createUsersList } from "./search-menu/search-menu-helpers";
-import { fetchUsers } from "./pagination/pagination-helper";
 
 header();
 createUserCard();
 createUsersList();
-fetchUsers();

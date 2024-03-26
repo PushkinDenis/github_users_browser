45268c24ba890cc905206d6f8a300c69f9d48463
Denis <denisp9831@gmail.com>
2024-03-26 16:06:54 +1000

    pagination and user card styles fixed

--- src/pagination/pagination.ts
+++ src/pagination/pagination.ts
@@ -1,14 +1,14 @@
 const pagination = (): string => {
   return `<ul class="pagination">
     <button class="pagination__btn pagination__min-btn">&lt</button>
-    <li class="pagination__li pagination__num">1</li>
-    <li class="pagination__li pagination__num">2</li>
-    <li class="pagination__li pagination__num">3</li>
-    <li class="pagination__li pagination__num">4</li>
-    <li class="pagination__li pagination__num">5</li>
-    <li class="pagination__li pagination__num">6</li>
+    <li class="pagination__li pagination__num" tabindex="0">1</li>
+    <li class="pagination__li pagination__num" tabindex="0">2</li>
+    <li class="pagination__li pagination__num" tabindex="0">3</li>
+    <li class="pagination__li pagination__num" tabindex="0">4</li>
+    <li class="pagination__li pagination__num" tabindex="0">5</li>
+    <li class="pagination__li pagination__num" tabindex="0">6</li>
     <li class="pagination__li ">...</li>
-    <li class="pagination__li ">10</li>
+    <li class="pagination__li pagination__total-num" tabindex="0">100</li>
     <button class="pagination__btn pagination__pls-btn">&gt</button>
   </ul>`;
 };
--- src/user-card/user-card.ts
+++ src/user-card/user-card.ts
@@ -5,10 +5,6 @@ const userCard = (user: GitHubUser): string => {
   user.location === null ? (user.location = "No location") : user.location;
   return `<div class="user-card">
     <div class="user-card__col">
-      <div class="user-card__label">Name</div>
-      <div class="user-card__name user-card__prop">${user.name}</div>
-    </div>
-    <div class="user-card__col">
       <div class="user-card__label">Login</div>
       <div class="user-card__log user-card__prop">${user.login}</div>
     </div>
@@ -21,13 +17,35 @@ const userCard = (user: GitHubUser): string => {
       <a href="${user.html_url}"class="user-card__url user-card__prop" target="_blank">${user.html_url}</a>
     </div>
     <div class="user-card__col">
-      <div class="user-card__label">Location</div>
-      <div class="user-card__loc user-card__prop">${user.location}</div>
-    </div>
-    <div class="user-card__col">
       <img src="${user.avatar_url} user-card__prop" alt="" class="user-card__avatar" />
     </div>
   </div>`;
 };
 
 export { userCard };
+
+// `<div class="user-card">
+//     <div class="user-card__col">
+//       <div class="user-card__label">Name</div>
+//       <div class="user-card__name user-card__prop">${user.name}</div>
+//     </div>
+//     <div class="user-card__col">
+//       <div class="user-card__label">Login</div>
+//       <div class="user-card__log user-card__prop">${user.login}</div>
+//     </div>
+//     <div class="user-card__col">
+//       <div class="user-card__label">ID</div>
+//       <div class="user-card__id user-card__prop">${user.id}</div>
+//     </div>
+//     <div class="user-card__col">
+//       <div class="user-card__label">URL</div>
+//       <a href="${user.html_url}"class="user-card__url user-card__prop" target="_blank">${user.html_url}</a>
+//     </div>
+//     <div class="user-card__col">
+//       <div class="user-card__label">Location</div>
+//       <div class="user-card__loc user-card__prop">${user.location}</div>
+//     </div>
+//     <div class="user-card__col">
+//       <img src="${user.avatar_url} user-card__prop" alt="" class="user-card__avatar" />
+//     </div>
+//   </div>`;
